ccc6ec5184b6c73e7b85a05842f8b78309a745ef
Sanzila Hossain <sanzila.tuya@gmail.com>
2025-09-30 13:23:35 +0200

    Connected with StepperProps

--- components/stepper/stepper.tsx
+++ components/stepper/stepper.tsx
@@ -1,14 +1,21 @@
-interface StepperProps {}
+interface StepperProps {
+    currentStep: number
+    totalSteps: number
+    steps: Step[]
+}
 
-export default function Stepper() {
-	/*TODO: Replace this with the actual Stepper implementation*/
-	return (
-		<div
-			className={
-				"h-[100px] border border-dashed border-gray-200 rounded flex justify-center items-center"
-			}
-		>
-			<code className={"text-2xl"}>{"<Stepper />"}</code>
-		</div>
-	);
+interface Step {
+    title: string
+}
+
+export default function Stepper({ currentStep, steps }: StepperProps) {
+    return (
+        <div className="flex justify-between items-center w-full">
+            {steps.map((step, index) => (
+                <div key={step.title} className="text-center">
+                    <p className="text-sm font-medium">{step.title}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
-import Button from "../components/repareo/button";
-import ButtonWrapper from "../components/repareo/buttonWrapper";
-import Header from "../components/repareo/header";
-import MainWrapper from "../components/repareo/mainWrapper";
-import StepperWrapper from "../components/repareo/stepperWrapper";
-import Stepper from "../components/stepper/stepper";
-import useStepper from "../hooks/useStepper";
+import Button from "../components/repareo/button"
+import ButtonWrapper from "../components/repareo/buttonWrapper"
+import Header from "../components/repareo/header"
+import MainWrapper from "../components/repareo/mainWrapper"
+import StepperWrapper from "../components/repareo/stepperWrapper"
+import Stepper from "../components/stepper/stepper"
+import { steps } from "../components/stepper/steps"
+import useStepper from "../hooks/useStepper"
 
 export default function Home() {
-	const { currentStep, handleNextStep } = useStepper();
-	return (
-		<>
-			<Header />
-			<MainWrapper>
-				<StepperWrapper>
-					{/*TODO: Make sure the Stepper handles clicks on the button*/}
-					<Stepper />
-				</StepperWrapper>
-				<ButtonWrapper>
-					<Button onClick={handleNextStep}>Next</Button>
-				</ButtonWrapper>
-			</MainWrapper>
-		</>
-	);
+    const { currentStep, handleNextStep, totalSteps } = useStepper()
+    return (
+        <>
+            <Header />
+            <MainWrapper>
+                <StepperWrapper>
+                    <Stepper
+                        currentStep={currentStep}
+                        totalSteps={totalSteps}
+                        steps={steps}
+                    />
+                </StepperWrapper>
+                <ButtonWrapper>
+                    <Button onClick={handleNextStep}>Next</Button>
+                </ButtonWrapper>
+            </MainWrapper>
+        </>
+    )
 }
