74424edbc18c2eed08560ecdbc1f52121f365c49
Sanzila Hossain <sanzila.tuya@gmail.com>
2025-09-30 12:50:40 +0200

    Populated steps

--- components/stepper/steps.ts
+++ components/stepper/steps.ts
@@ -1,5 +1,10 @@
 interface Step {
-	title: string;
+    title: string
 }
 
-export const steps: Step[] = [];
+export const steps: Step[] = [
+    { title: "Service" },
+    { title: "Termin" },
+    { title: "Fahrzeug" },
+    { title: "Kontakt" },
+]
