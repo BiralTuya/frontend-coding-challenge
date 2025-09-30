8cec5723ed9bb3b0fa3e6a80f42c01e501cdd30b
Sanzila Hossain <sanzila.tuya@gmail.com>
2025-09-30 12:59:11 +0200

    Modified useStepper hook

--- hooks/useStepper.ts
+++ hooks/useStepper.ts
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState } from "react"
+import { steps } from "../components/stepper/steps"
 
 export default function useStepper() {
-	const [currentStep, setCurrentStep] = useState<number>(0);
-	function handleNextStep() {
-		setCurrentStep((prev) => {
-			return prev + 1;
-		});
-	}
-	return { currentStep, handleNextStep };
+    const [currentStep, setCurrentStep] = useState<number>(0)
+    const totalSteps = steps.length
+
+    function handleNextStep() {
+        setCurrentStep((prev) => {
+            if (prev < totalSteps - 1) {
+                return prev + 1
+            }
+            return prev
+        })
+    }
+
+    return { currentStep, handleNextStep, steps, totalSteps }
 }
